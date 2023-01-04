6d43b289b7e57d0fa710ee7ce9df754934bec62d
Aslan <asln86@mail.ru>
2023-01-05 01:32:18 +0300

    создал объект с описанием персонажа игры с небольшим функционалом

---
+++ soldier.js
@@ -0,0 +1,12 @@
+const soldier = {
+    name: 'Халк',
+    health: 10,
+    weapon: {
+        weapon_name: 'ПП',
+        ammo: 30,
+    },
+    supplies: 3,
+    fire: function () {},
+    recharge: function () {},
+    hurt: function () {},
+};

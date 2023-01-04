96423968750c8b4d749aa01fcc356c061b042711
Aslan <asln86@mail.ru>
2023-01-05 01:46:25 +0300

    дополнил функционал

--- soldier.js
+++ soldier.js
@@ -6,7 +6,28 @@ const soldier = {
         ammo: 30,
     },
     supplies: 3,
-    fire: function () {},
-    recharge: function () {},
-    hurt: function () {},
+    fire: function () {
+        this.weapon.ammo--
+        console.log('бах-бах');
+        if (this.weapon.ammo === 0) {
+            console.log('обойма пуста. Перезаредитесь');
+        }
+    },
+    recharge: function () {
+        this.weapon.ammo = 30;
+        this.supplies--;
+        console.log('перезарядка...');
+        if (this.supplies === 0) {
+            console.log('не осталось припасов');
+        }
+    },
+    hurt: function () {
+        this.health--;
+        if (this.health === 0){
+            console.log('Ты проиграл');
+        } 
+    },
 };
+
+
+
